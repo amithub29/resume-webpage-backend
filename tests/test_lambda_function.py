03156d6698b1592fa160ec7b880a7642ba3f79c5
Amit Mehta <amitmehta.aws@gmail.com>
2024-09-30 20:33:31 +0530

    Test Case changes

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -1,10 +1,10 @@
 import unittest
-from resume_backend import lambda_function
+from resume_backend.lambda_function import lambda_handler
 
 class TestLambda(unittest.TestCase):
 
     def test_lambda_handler(self):
-        response = lambda_function.lambda_handler(None, None)
+        response = lambda_handler(None, None)
         self.assertEqual(response['statusCode'], 200)
 
 if __name__ == '__main__':
